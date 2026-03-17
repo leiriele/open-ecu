97d04a180e9119258718640153c228d5b1d8ee7f
leiriele <leiricorrea@gmail.com>
2026-03-17 20:32:41 -0300

    fix(): ajuste na variavel de aambiente

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:3000";
+//const API_URL = "http://localhost:3000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 
 async function handleResponse<T>(response: Response): Promise<T> {
   if (!response.ok) {
